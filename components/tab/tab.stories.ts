ea8aebab377759021f1554b366d1eddabf17e545
mojahige <13248811+mojahige@users.noreply.github.com>
2023-12-30 12:53:29 +0900

    Add story for tab

--- components/tab/tab.stories.ts
+++ components/tab/tab.stories.ts
@@ -14,6 +14,77 @@ const meta: Meta = {
 export default meta;
 type Story = StoryObj;
 
+export const Example: Story = {
+  render: () => html`
+  <div is="x-tab">
+    <h1 id="tab">Tab</h1>
+
+    <div role="tablist" aria-labelledby="tab">
+      <button
+        id="tab-1"
+        type="button"
+        role="tab"
+        tabindex="0"
+        aria-selected="true"
+        aria-controls="tabpanel-1"
+      >
+        Tab 1
+      </button>
+
+      <button
+        id="tab-2"
+        type="button"
+        role="tab"
+        tabindex="-1"
+        aria-selected="false"
+        aria-controls="tabpanel-2"
+      >
+        Tab 2
+      </button>
+
+      <button
+        id="tab-3"
+        type="button"
+        role="tab"
+        tabindex="-1"
+        aria-selected="false"
+        aria-controls="tabpanel-3"
+      >
+        Tab 3
+      </button>
+    </div>
+
+    <div
+      id="tabpanel-1"
+      class="is-shown"
+      role="tabpanel"
+      tabindex="0"
+      aria-labelledby="tab-1"
+    >
+      <p>Tab Panel 1</p>
+    </div>
+
+    <div
+      id="tabpanel-2" 
+      role="tabpanel"
+      tabindex="0"
+      aria-labelledby="tab-2"
+    >
+      <p>Tab Panel 2</p>
+    </div>
+
+    <div
+      id="tabpanel-3"
+      role="tabpanel"
+      tabindex="0" 
+      aria-labelledby="tab-3"
+    >
+      <p>Tab Panel 3</p>
+    </div>
+  </div>
+  `,
+};
+
 export const AutoInitialSelect: Story = {
   render: () => html`
   <div is="x-tab">
