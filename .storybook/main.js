905df196a8fc05665a7a9ff8660badb04a457616
mojahige <13248811+mojahige@users.noreply.github.com>
2024-01-05 23:36:17 +0900

    Add Storybook accessibility addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,11 @@ const config = {
     "../elements/**/*.mdx",
     "../elements/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-a11y",
+  ],
   framework: {
     name: "@storybook/web-components-vite",
     options: {},
